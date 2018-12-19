61cbe0bdc224b06b02fa1d8ab67b7c2043e320f6
Twilit <kinlongsha@hotmail.co.uk>
2018-12-19 12:15:18 +0000

    0.233
    Stamina Bar updatesize in progress

--- Assets/Scripts/BarScript.cs
+++ Assets/Scripts/BarScript.cs
@@ -18,15 +18,28 @@ public class BarScript : MonoBehaviour
     float baseMax;
     Entity entityStats;
 
+    RectTransform barRect;
+    RectTransform barMaskRect;
+    RectTransform mainFillRect;
+    RectTransform backFillRect;
+
     void Start()
     {
         entityStats = entity.GetComponent<Player>();
+
+        barRect = GetComponent<RectTransform>();
+        barMaskRect = transform.GetChild(0).GetComponent<RectTransform>();
+        backFillRect = transform.GetChild(0).transform.GetChild(0).GetComponent<RectTransform>();
+        mainFillRect = transform.GetChild(0).transform.GetChild(1).GetComponent<RectTransform>();
+
         baseMax = entityStats.MaxStamina;
     }
 
 	void LateUpdate () 
 	{
         UpdateBarFill(entityStats.CurrentStamina, entityStats.MaxStamina);
+
+        UpdateBarSize();
     }
 
     void UpdateBarFill(float current, float max)
@@ -52,9 +65,26 @@ public class BarScript : MonoBehaviour
 
     void UpdateBarSize()
     {
+        float offset = 0;
+
         if (baseMax != entityStats.MaxStamina)
         {
+            offset = entityStats.MaxStamina - baseMax;
+            float sizeInc = ((offset / baseMax) * (barRect.sizeDelta.x));
 
+            barRect.sizeDelta = new Vector2((barRect.sizeDelta.x + sizeInc), barRect.sizeDelta.y);
+            barRect.anchoredPosition = new Vector2(barRect.anchoredPosition.x + sizeInc/2, barRect.anchoredPosition.y);
+
+            /*barMaskRect.sizeDelta = new Vector2(barMaskRect.sizeDelta.x + ((offset / baseMax) * (barRect.sizeDelta.x)), barMaskRect.sizeDelta.y);
+            barMaskRect.anchoredPosition = new Vector2(barMaskRect.anchoredPosition.x + ((offset / baseMax) * (barRect.sizeDelta.x)) / 2, barMaskRect.anchoredPosition.y);
+
+            backFillRect.sizeDelta = new Vector2(backFillRect.sizeDelta.x + ((offset / baseMax) * (barRect.sizeDelta.x)), backFillRect.sizeDelta.y);
+            backFillRect.anchoredPosition = new Vector2(backFillRect.anchoredPosition.x + ((offset / baseMax) * (barRect.sizeDelta.x)) / 2, backFillRect.anchoredPosition.y);
+
+            mainFillRect.sizeDelta = new Vector2(mainFillRect.sizeDelta.x + ((offset / baseMax) * (barRect.sizeDelta.x)), mainFillRect.sizeDelta.y);
+            mainFillRect.anchoredPosition = new Vector2(mainFillRect.anchoredPosition.x + ((offset / baseMax) * (barRect.sizeDelta.x)) / 2, mainFillRect.anchoredPosition.y);*/
+
+            baseMax = entityStats.MaxStamina;
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,5 +28,10 @@ public class Player : Entity
         }
 
         //print(currentStamina + "/" + maxStamina);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SetStats(800, 200, 40, 230, 120, 0);
+        }
     }
 }
